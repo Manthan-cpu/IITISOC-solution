f1f2916c6b67b7c1c7b2f81d057fe2fa6910151b
Aryan Jain <aryanj2615@gmail.com>
2025-06-25 20:11:28 +0530

    Update fetchinstruction.v

--- src/fetchinstruction.v
+++ src/fetchinstruction.v
@@ -16,12 +16,12 @@ input wire clk,
 
     
     reg [7:0] PC;
-
+    reg halt;
     
     reg [15:0] instruction_memory [0:255];
 
     initial begin
-        $readmemh("instruction .mem", instruction_memory);
+        $readmemh("C:/Users/Aryan jain/programs/8-bit-pipelined-microprocessor/8-bit-pipelined-microprocessor.sim/sim_1/behav/xsim/xsim.dir/instruction.mem", instruction_memory);
         
     end
 
@@ -29,9 +29,10 @@ input wire clk,
         if (reset) begin
             PC <= 8'b0;
             PC_out <= 8'b0;
+            halt <= 1'b0;
             instruction <= 16'h0000;
             valid <= 1'b0;
-        end else if (stall) begin
+        end else if (stall || halt) begin
             
             valid <= 1'b0; 
         end else begin
@@ -45,9 +46,10 @@ input wire clk,
             instruction <= flush ? 16'h0000 : instruction_memory[PC];
             PC_out <= PC;
             valid <= ~flush;
+            
+            if (!flush && instruction_memory[PC][15:12] == 4'b1111) begin
+            halt <= 1'b1; end
         end
     end
-
-
   
 endmodule
