1c2c20d627dbeb494c77950c591996c54794d3f8
ashmita2212 <ashmita3823@gmail.com>
2025-07-07 10:19:06 +0530

    Add files via upload

---
+++ src/Stage_wb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+
+module stage_WB (
+    input  wire        RegWrite_WB,     
+    input  wire        ResultSrc_WB,    
+    input  wire [7:0]  alu_result_WB,   
+    input  wire [7:0]  mem_data_WB,     
+    input  wire [2:0]  rd_WB,           
+
+    output wire        RegWrite_final,  
+    output wire [7:0]  write_data_WB,   
+    output wire [2:0]  rd_final      
+);
+
+   
+    writeback_stage wb_mux (
+        .ResultSrc(ResultSrc_WB),
+        .alu_result(alu_result_WB),
+        .mem_data(mem_data_WB),
+        .writeback_data(write_data_WB)
+    );
+
+    assign RegWrite_final = RegWrite_WB;
+    assign rd_final       = rd_WB;
+
+endmodule
