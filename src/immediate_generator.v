a267c78fc000bf96a4af54b24bbca99e31f6fc7a
Manthan-cpu <ee240002036@iiti.ac.in>
2025-07-15 17:19:52 +0530

    Update immediate_generator.v

--- src/immediate_generator.v
+++ src/immediate_generator.v
@@ -11,6 +11,7 @@ module immediate_generator(
             2'b00: imm_out = instruction[11:4]; // 8-bit immediate (LDI, JMP)
             2'b01: imm_out = {{2{instruction[5]}}, instruction[5:0]}; // 6-bit signed (LOAD, STORE, BRANCH)
             2'b10: imm_out = {5'b00000, instruction[5:3]}; // 3-bit(SHIFT)
+            2'b11: imm_out = {{3{instruction[4]}},instruction[4:0]};//5-bit(ADDI)
             default: imm_out = 8'b0;
         endcase
     end
