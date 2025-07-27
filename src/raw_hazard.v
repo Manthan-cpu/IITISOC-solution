fdda44a10c44e5a952fe7a35180af1ebd749d053
Manthan-cpu <ee240002036@iiti.ac.in>
2025-07-27 22:25:46 +0530

    Update raw_hazard.v

--- src/raw_hazard.v
+++ src/raw_hazard.v
@@ -14,19 +14,26 @@ module data_memory_hazard(
     output reg [1:0] forward_B
 );
 
+    reg EX_MEM_regwrite_d;
+    reg MEM_WB_regwrite_d;
+
+    // Shift EX_MEM_regwrite and MEM_WB_regwrite by one clock cycle
     always @(posedge clk) begin
-        
-        if (EX_MEM_regwrite && (EX_MEM_rd != 3'b000) && (EX_MEM_rd == rs1))
+        EX_MEM_regwrite_d <= EX_MEM_regwrite;
+        MEM_WB_regwrite_d <= MEM_WB_regwrite;
+    end
+
+    always @(*) begin
+        if (EX_MEM_regwrite_d && (EX_MEM_rd != 3'b000) && (EX_MEM_rd == rs1))
             forward_A = 2'b10;
-        else if (MEM_WB_regwrite && (MEM_WB_rd != 3'b000) && (MEM_WB_rd == rs1))
+        else if (MEM_WB_regwrite_d && (MEM_WB_rd != 3'b000) && (MEM_WB_rd == rs1))
             forward_A = 2'b01;
         else
             forward_A = 2'b00;
 
-       
-        if (EX_MEM_regwrite && (EX_MEM_rd != 3'b000) && (EX_MEM_rd == rs2))
+        if (EX_MEM_regwrite_d && (EX_MEM_rd != 3'b000) && (EX_MEM_rd == rs2))
             forward_B = 2'b10;
-        else if (MEM_WB_regwrite && (MEM_WB_rd != 3'b000) && (MEM_WB_rd == rs2))
+        else if (MEM_WB_regwrite_d && (MEM_WB_rd != 3'b000) && (MEM_WB_rd == rs2))
             forward_B = 2'b01;
         else
             forward_B = 2'b00;
