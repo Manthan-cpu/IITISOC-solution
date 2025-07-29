c5d9628a23d2f468eacf87d8b2145a201d3cebd3
Aryan Jain <aryanj2615@gmail.com>
2025-07-29 23:14:17 +0530

    Update immediate_generator.v

--- src/immediate_generator.v
+++ src/immediate_generator.v
@@ -1,3 +1,4 @@
+
 `timescale 1ns / 1ps
 
 module immediate_generator(
@@ -9,12 +10,12 @@ module immediate_generator(
     always @(*) begin
         case (ImmSrc)
             2'b00: case(instruction[15:12])
-                4'b1010: imm_out = instruction[8:1]; // JMP
-                4'b1101: imm_out = instruction[11:4]; //LDI
+                4'b1010: imm_out = instruction[8:1]; // LDI
+                4'b1101: imm_out = instruction[11:4]; //JMP
                    endcase
             2'b01: imm_out = {{2{instruction[5]}}, instruction[5:0]}; // LOAD, STORE, BEQ, BNE
             2'b10: imm_out = {5'b00000, instruction[5:3]}; //SHIFT
-            2'b11: imm_out = {{3{instruction[1]}}, instruction[5:1]}; //ADDI
+            2'b11: imm_out = {{3{instruction[5]}}, instruction[5:1]}; //ADDI
             default: imm_out = 8'b0;
         endcase
     end
