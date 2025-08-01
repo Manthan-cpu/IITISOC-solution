6dff39317ae04c6cef226f9d99d64346ba337890
Manthan-cpu <ee240002036@iiti.ac.in>
2025-08-01 21:52:01 +0530

    Update Fetch_pipeline.v

--- src/Fetch_pipeline.v
+++ src/Fetch_pipeline.v
@@ -17,20 +17,21 @@ module fetch789(
 );
 
     reg [7:0] PC;
+    reg [7:0] branch_target_d;
    
     reg [15:0] instruction_memory [0:255];
 
     initial begin
-        $readmemb("instruction.mem", instruction_memory);
+        $readmemb("instructions.mem", instruction_memory);
     end
-
+    
     always @(posedge clk or posedge reset) begin
+    branch_target_d <= branch_target;
         if (reset) begin
             PC <= 8'b0;
             PC_out <= 8'b0;
             instruction <= 16'b0;
             valid <= 1'b0;
-        
             update <= 1'b0;
         end 
         
@@ -42,22 +43,12 @@ module fetch789(
             valid <= ~flush;
             update <= 0;
 
-            // if (instruction_memory[PC][15:12] == 4'b1111)
-            //     halt <= 1'b1;
 
-//            if (jump) begin
-//                PC <= instruction_memory[PC][11:4];
-//            end
-            if (PC_sel) begin
-                PC <= branch_target;
-                update <= 1;
+            if (flush) begin
+                PC <= branch_target_d;
+                instruction <= 16'b1110000000000000 ;
             end
-            else if (flush) begin
-                PC <= branch_target;
-            end
-//            else if (predict_taken) begin
-//                PC <= branch_target;
-//            end
+
             else begin
                 PC <= PC + 1;
             end
@@ -66,8 +57,5 @@ module fetch789(
             valid <= 1'b0;
         end
     end
-    
-    
-    
-
+ 
 endmodule
