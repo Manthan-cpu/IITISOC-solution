a9d89d29e956ad47f3e943e0bf848e473e8f50ed
Manthan-cpu <ee240002036@iiti.ac.in>
2025-08-03 17:25:50 +0530

    Update Stage_MEM.v

--- src/Stage_MEM.v
+++ src/Stage_MEM.v
@@ -3,6 +3,7 @@
 module stage_MEM(
     input  wire        clk,
     input  wire        reset,
+    input  wire        flush,
     input  wire        MemRead_MEM,
     input  wire        MemWrite_MEM,
     input  wire        ResultSrc_MEM,
@@ -29,6 +30,7 @@ module stage_MEM(
     memory_stage data_mem (
         .clk(clk),
         .reset(reset),
+        .flush(flush),
         .MemRead(MemRead_MEM),
         .MemWrite(MemWrite_MEM),
         .alu_result(alu_result_MEM),
@@ -40,7 +42,7 @@ module stage_MEM(
     assign  rd_WB = rd_MEM;
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            mem_data_out   <= 8'b0;
+//            mem_data_out   <= 8'b0;
             alu_result_out <= 8'b0;
             ResultSrc_WB   <= 1'b0;
             Regwrite_WB    <= 1'b0;
@@ -48,12 +50,16 @@ module stage_MEM(
         end
         
         else begin
-            mem_data_out   <= mem_read_data;
+//            mem_data_out   <= mem_read_data;
             alu_result_out <= alu_result_MEM;
             ResultSrc_WB   <= ResultSrc_MEM;
             Regwrite_WB    <= RegWrite_MEM;
             rd_WB_d        <= rd_MEM;
         end
     end
+       always @(*)
+       begin
+       mem_data_out   = mem_read_data ;
+       end
 
 endmodule
